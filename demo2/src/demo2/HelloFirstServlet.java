88654dbc7e8d83edad538f1e5b6c5492a5b58218
vallis <vallis726@gmail.com>
2017-10-15 23:21:39 +0800

    fist

---
+++ demo2/src/demo2/HelloFirstServlet.java
@@ -0,0 +1,20 @@
+package demo2;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloFirstServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		arg1.getOutputStream().write("helloworld".getBytes());
+
+	}
+
+}
